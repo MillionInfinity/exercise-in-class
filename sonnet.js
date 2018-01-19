19230344d0cce063e175a6306f75658deb1d7872
Million Yemane <wedi_ngus@yahoo.com>
2018-01-19 13:42:11 -0600

    final commit

--- sonnet.js
+++ sonnet.js
@@ -1,13 +1,10 @@
 console.log("test 123");
 
-var str = document.getElementById('sonnet').innerText;
-var orphanIndex = str.indexOf("orphans");
+var str = document.getElementById('sonnet').innerHTML;
+var orphanIndex = str.indexOf("orphan");
 console.log(orphanIndex);
-
-var orphanLastIndex = sonnet.lastIndexOf(".");
-console.log(orphanLastIndex);
-
-document.getElementById("sonnet").innerHTML = document.getElementById("sonnet").innerHTML.replace("winter", "yuletide");
-document.getElementById("sonnet").innerHTML = document.getElementById("sonnet").innerHTML.replace(/the/ig,"a large");
-
+var orphanLength= str.lenght;
+var orphanreplace = str.replace("winter", "yuletide");
+var orphanReeplace = orphanreplace.split("the").join("a large");
+document.getElementById('sonnet').innerHTML = orphanValue + sonnetLength + sonnetReplaceWord;
 
